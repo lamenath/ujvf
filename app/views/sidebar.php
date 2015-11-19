46e9c915a0ae464dc3f1f1f0a88bd3f639d0e287
Nathanael Lamelliere <nla@zengularity.com>
2015-11-19 09:48:17 +0100

    removing blog from menu

--- app/views/sidebar.php
+++ app/views/sidebar.php
@@ -1,9 +1,10 @@
-<div class="search">
+
+<!--<div class="search">
     <div class="search-icon">
         <i class="fa fa-search"></i>
     </div>
-    <?php get_search_form() ?>
-</div>
+    <?php get_search_form() ?> 
+</div>-->
 
 <div class="sidebar-section">
     <img src="https://prismic-io.s3.amazonaws.com/lavieenjaune/7299080fd2dbfb32cd8cdcfbcf3328745a3ed1e1_logo-ujvf-fond-transparent-1.png"/>
@@ -21,12 +22,11 @@
     </div>
 <?php } ?>
 
-<div class="sidebar-section">
+<!-- <div class="sidebar-section">
     <h3>Archives</h3>
     <ul>
         <?php foreach(get_calendar() as $entry) { ?>
             <li><a href="<?= $entry['link'] ?>"><?= $entry['label'] ?></a></li>
         <?php } ?>
     </ul>
-</div>
-
+</div> -->
