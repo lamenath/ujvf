83b856a67c867f054c7f346366057eae569dd305
Nathanael Lamelliere <nla@zenexity.com>
2016-12-17 15:45:20 +0100

    change meta title and desc
    remove prismic.io mention in meta title and desc

--- config.php
+++ config.php
@@ -29,8 +29,8 @@ define('PAGE_SIZE', 10);
 /*
  * Your site metadata
  */
-define('SITE_TITLE', 'prismic.io Website Starter');
-define('SITE_DESCRIPTION', 'This is a sample theme using Bootstrap');
+define('SITE_TITLE', 'Union des Jeunes Vietnamiens de France');
+define('SITE_DESCRIPTION', 'Union des Jeunes Vietnamiens de France');
 define('ADMIN_EMAIL', '');
 
 /*
